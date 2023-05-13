dbcb5724d08c3c86d0d6bcdd4293b25eb30b1607
Bekhruz2021 <92889070+Bekhruz2021@users.noreply.github.com>
2023-05-13 16:32:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function bedroom()
+{
+window.location = "bedroom.html";
+}
+
+function TV()
+{
+window.location = "tv.html";
+}
+
+function desk()
+{
+window.location = "desk.html";
+}
+
+function bottle()
+{
+window.location = "bottle.html";
+}
+
+function laptop()
+{
+window.location = "laptop.html";
+}
